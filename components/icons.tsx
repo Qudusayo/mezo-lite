176d29c6ff22dd6a8766f4a37c8f01cb5ab31e3e
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-10-08 02:00:58 +0100

    Add GiftIcon and LinkIcon components to icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -85,6 +85,29 @@ const DiamondIcon = (props: SvgProps) => (
   </Svg>
 );
 
+const GiftIcon = (props: SvgProps) => (
+  <Svg width={24} height={24} viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <Path d="M0 0h24v24H0z" fill="none" />
+    <Path d="M11 14v8H7a3 3 0 0 1-3-3v-4a1 1 0 0 1 1-1zm8 0a1 1 0 0 1 1 1v4a3 3 0 0 1-3 3h-4v-8zM16.5 2a3.5 3.5 0 0 1 3.163 5H20a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-7V7h-2v5H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h.337A3.5 3.5 0 0 1 4 5.5C4 3.567 5.567 2 7.483 2c1.755-.03 3.312 1.092 4.381 2.934l.136.243c1.033-1.914 2.56-3.114 4.291-3.175zm-9 2a1.5 1.5 0 0 0 0 3h3.143C9.902 5.095 8.694 3.98 7.5 4m8.983 0c-1.18-.02-2.385 1.096-3.126 3H16.5a1.5 1.5 0 1 0-.017-3" />
+  </Svg>
+);
+
+const LinkIcon = (props: SvgProps) => (
+  <Svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}>
+    <Path d="M0 0h24v24H0z" stroke="none" />
+    <Path d="m9 15 6-6m-4-3 .463-.536a5 5 0 0 1 7.071 7.072L18 13m-5 5-.397.534a5.07 5.07 0 0 1-7.127 0 4.97 4.97 0 0 1 0-7.071L6 11" />
+  </Svg>
+);
+
 const LoacationDollarIcon = (props: SvgProps) => (
   <Svg
     width={24}
@@ -198,10 +221,12 @@ export {
   BoltIcon,
   DiamondIcon,
   LoacationDollarIcon,
+  LinkIcon,
   WalletIcon,
   ScanIcon,
   PhoneIcon,
   PlusIcon,
   RepeatIcon,
   QrCodeIcon,
+  GiftIcon,
 };
